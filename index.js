228ee1522b3cf1ea96c213214cc9f11af22c574d
superkraft388 <wislinkhonde@hotmail.com>
2025-09-13 10:04:06 -0500

    Add a greeting in English forbetter user experience

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Archivo principal del proyecto
 
 console.log('Bienvenido al proyecto Branquias de Características. ¡Esperamos que lo disfrutes!');
+console.log('Welcome to the Branquias de Características project. We hope you enjoy it!');
 
 // Aquí se podrían importar y usar las herramientas relacionadas con la gestión de ramas,
 // integración continua, etc.
