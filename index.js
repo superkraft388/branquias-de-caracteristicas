df69b3d46e1251d7a0365400b2bd5691c5242749
superkraft388 <wislinkhonde@hotmail.com>
2025-09-13 10:05:09 -0500

    Improve welcome message format and add a todo comment for future enhancements.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Archivo principal del proyecto
 
-console.log('Bienvenido al proyecto Branquias de Características. ¡Esperamos que lo disfrutes!');
-console.log('Welcome to the Branquias de Características project. We hope you enjoy it!');
+console.log('=== Bienvenido al proyecto Branquias de Características ===');
+console.log('=== Welcome to the Branquias de Características project ===');
+console.log('¡Esperamos que lo disfrutes!');
+console.log('We hope you enjoy it!');
 
 // Aquí se podrían importar y usar las herramientas relacionadas con la gestión de ramas,
 // integración continua, etc.
+// TODO: Implement branch management and CI tools in future versions.
