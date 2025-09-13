d6ae43f5f4a5572565119124223d1793edd5ba2a
superkraft388 <wislinkhonde@hotmail.com>
2025-09-13 10:03:24 -0500

    Improve welcome message formatting.

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 
 console.log('Bienvenido al proyecto Branquias de Características. ¡Esperamos que lo disfrutes!');
 
-// Aquí se podrían importar y usar las herramientas relacionadas con la gestión de ramas, integración continua, etc.
+// Aquí se podrían importar y usar las herramientas relacionadas con la gestión de ramas,
+// integración continua, etc.
