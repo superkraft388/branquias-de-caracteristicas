fec953551c215223866ba8c5a9c8562abc1cf294
superkraft388 <wislinkhonde@hotmail.com>
2025-09-13 10:01:24 -0500

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// Archivo principal del proyecto
+
+console.log('Bienvenido al proyecto Branquias de Características');
+
+// Aquí se podrían importar y usar las herramientas relacionadas con la gestión de ramas, integración continua, etc.
